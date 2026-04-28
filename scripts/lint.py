36f3eacb8240b0a3322a3b57f0193166051ac1e7
Alexander Bantyev <balsoft@balsoft.ru>
2026-04-28 10:36:30 +0000

    Format script & stuff (#97)
    * scripts/lint.py: fmt
    
    * fix script & stuff
    
    * Update actions
    
    * Add a message for no issues found
    
    * Another random fix

--- scripts/lint.py
+++ scripts/lint.py
@@ -11,15 +11,14 @@ import requests
 import json
 import logging
 
-ns = {
-    "josm": "http://josm.openstreetmap.de/tagging-preset-1.0"
-}
+ns = {"josm": "http://josm.openstreetmap.de/tagging-preset-1.0"}
 
 DEFAULT_LANG = "ka"
-LANGS = [ "en", "ru", "ka" ]
+LANGS = ["en", "ru", "ka"]
+
 
 class Issue:
-    item : Element | None = None
+    item: Element | None = None
 
     def __init__(self, message, severity=logging.WARN, item=None, tag=None):
         self.message = message
@@ -35,29 +34,36 @@ class Issue:
         else:
             return "<unknown>"
 
+
 def issues_with_item(item, issues):
     for issue in issues:
         issue.item = item
         yield issue
+
 
 def issues_with_tag(tag, issues):
     for issue in issues:
         issue.tag = tag
         yield issue
 
+
 def item_tags(item: Element) -> dict:
     tags = {}
-    for key in item.findall('josm:key', ns):
+    for key in item.findall("josm:key", ns):
         tags[key.attrib["key"]] = key.attrib["value"]
     return tags
 
+
 wikidata_cache = {}
+
 
 def fetch_wikidata_qid(qid: str) -> dict:
     if qid in wikidata_cache:
         return wikidata_cache[qid]
     else:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+        }
         url = f"https://www.wikidata.org/w/rest.php/wikibase/v1/entities/items/{qid}"
         resp = requests.get(url, headers=headers)
         while resp.status_code == 429:
@@ -75,17 +81,20 @@ def match_values(name1, value1, name2, value2) -> Generator[Issue]:
     if value1 is not None and value2 is not None and value1 != value2:
         yield Issue(f"{name1} ({value1}) does not match {name2} ({value2})")
 
+
 def check_wikidata_tag(wd: dict, item, key: str, lang: str) -> Generator[Issue]:
     for k in filter(lambda x: x.attrib["key"] == key, item.findall("josm:key", ns)):
         name = k.attrib["value"]
-        if not (
-            "labels" not in wd or lang not in wd["labels"] or name == wd["labels"][lang]
-            or "aliases" not in wd or lang not in wd["aliases"] or name in wd["aliases"][lang]
-            ):
-                yield Issue(
-                            f"{key} ({name}) is neither a label ({wd.get("labels", {}).get(lang, "<no label>")}) nor an alias ({", ".join(wd.get("aliases", {}).get(lang, []))}) of wikidata item {wd["id"]} in language {lang}; are you sure {key}={wd["id"]} is the correct tag?",
-                            tag = item,
-                        )
+        if name != wd.get("labels", {}).get(lang, None) and name not in wd.get(
+            "aliases", {}
+        ).get(lang, []):
+            yield Issue(
+                f"{key} ({name}) is neither a label ({wd.get("labels", {}).get(lang, "<no label>")}) nor an alias ({", ".join(wd.get("aliases", {}).get(lang, []))}) of wikidata item {wd["id"]} in language {lang}; if you are sure {key}={wd["id"]} is correct, add a corresponding label or alias to the item",
+                tag=item,
+            )
+    if False:
+        yield None
+
 
 def check_wikidata_family(item: Element, tag_family: str):
     qid = item_tags(item).get(f"{tag_family}:wikidata", None)
@@ -95,22 +104,29 @@ def check_wikidata_family(item: Element, tag_family: str):
             key = f"{tag_family}:{lang}"
             yield from check_wikidata_tag(wd, item, key, lang)
 
+
 def check_wikidata(item):
     yield from check_wikidata_family(item, "brand")
     yield from check_wikidata_family(item, "operator")
+
 
 def check_tag_kartuli(item: Element, tag_family: str):
     tags = item_tags(item)
     l = f"{tag_family}:{DEFAULT_LANG}"
     if tag_family in tags and l in tags:
-        yield from issues_with_tag(item, match_values(f"tag {tag_family}", tags[tag_family], f"tag {l}", tags[l]))
+        yield from issues_with_tag(
+            item,
+            match_values(f"tag {tag_family}", tags[tag_family], f"tag {l}", tags[l]),
+        )
+
 
 def check_names_kartuli(item):
     yield from check_tag_kartuli(item, "brand")
     yield from check_tag_kartuli(item, "operator")
 
+
 def check_group(group: Element, diff_map: dict):
-    for item in group.findall('josm:item', ns):
+    for item in group.findall("josm:item", ns):
         name = item.attrib["name"]
         if name not in diff_map or item_tags(item) != diff_map[name]:
             for check in [check_names_kartuli, check_wikidata]:
@@ -118,35 +134,48 @@ def check_group(group: Element, diff_map: dict):
         else:
             logging.debug(f"Item {name} has not changed, skip")
 
-    for subgroup in group.findall('josm:group', ns):
+    for subgroup in group.findall("josm:group", ns):
         yield from check_group(subgroup, diff_map)
+
 
 def prepare_item_map(group: Element):
     map = {}
-    for item in group.findall('josm:item', ns):
+    for item in group.findall("josm:item", ns):
         map[item.attrib["name"]] = item_tags(item)
 
-    for subgroup in group.findall('josm:group', ns):
+    for subgroup in group.findall("josm:group", ns):
         map = map | prepare_item_map(subgroup)
 
     return map
 
+
 def __main__():
     diff_map = {}
     if len(sys.argv) > 2:
-        diff_group = lxml.etree.parse(open(sys.argv[2], "r")).getroot().find("josm:group", ns)
+        diff_group = (
+            lxml.etree.parse(open(sys.argv[2], "r")).getroot().find("josm:group", ns)
+        )
         if diff_group is not None:
             diff_map = prepare_item_map(diff_group)
 
     exit_code = 0
 
     input_file = sys.argv[1]
-    root_group = lxml.etree.parse(open(input_file, "r")).getroot().find("josm:group", ns)
+    root_group = (
+        lxml.etree.parse(open(input_file, "r")).getroot().find("josm:group", ns)
+    )
+
     if root_group is not None:
         for issue in check_group(root_group, diff_map):
-            print(f"{input_file}#{issue.location()}: {logging.getLevelName(issue.severity)}: {issue.item.attrib["name"]}: {issue.message}")
+            print(
+                f"{input_file}#{issue.location()}: {logging.getLevelName(issue.severity)}: {issue.item.attrib["name"]}: {issue.message}"
+            )
             exit_code = 1
 
+    if exit_code == 0:
+        print("No issues found")
+
     exit(exit_code)
+
 
 __main__()
